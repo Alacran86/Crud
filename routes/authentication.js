cb33947e5accc0343f78749cd5200af226435a11
Alacran86 <alacran.mlr@gmail.com>
2021-08-30 20:15:31 -0300

    App Expense Control

---
+++ routes/authentication.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var pool = require('../bd');
+
+// router.get('/', (req, res)=>{
+//     res.render('signup');
+
+
+// });
+
+router.get('/signup', (req, res)=>{
+    res.render('signup');
+
+
+});
+
+router.post('/signup', async (req, res)=>{
+    let {fullname, username, password} = req.body;
+    let newOperation = {
+        fullname,
+        username,
+        password
+    };
+    
+    await pool.query('INSERT INTO users set ?' , [newOperation]);
+    res.render('success',{mensaje:"Se ha registrado correctamente!!"});
+});
+    
+   
+    
+
+router.get('/signin', (req, res)=>{
+    res.render('signin');
+
+
+});
+
+// Averiguar como hacer para que cuando se loguee con su clave y usuario me muestre el msj
+
+
+router.post('/signin', async (req, res)=>{
+    
+    
+    let username = await (pool.query("select * from username where User_ID = '" + 
+                            req.params.User_ID + "'"));
+
+    
+    res.render('success',{mensaje:"Ha iniciado sesion"});
+});
+
+
+
+module.exports = router;
